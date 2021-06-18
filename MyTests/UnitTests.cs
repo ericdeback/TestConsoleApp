97732541305244c08ed9e059cffa3280dbbf583a
ericdeback <1loveGithub>
2021-06-18 13:20:57 +0100

    Add project files.

---
+++ MyTests/UnitTests.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp
+{
+    public class Tests
+    {
+
+        Program pg;
+
+        [SetUp]
+        public void Setup()
+        {
+            pg = new Program();
+        }
+
+        [Test]
+        public void Test1()
+        {
+            pg.doSomething();
+            Assert.Pass();
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        //static void Main(string[] args)
+        //{
+        //    Console.WriteLine("Selenium C#");
+        //}
+
+        public void doSomething()
+        {
+            Console.WriteLine("foo");
+        }
+
+    }
+}
